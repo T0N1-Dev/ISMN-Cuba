b2f4ed0019c9701a3679cb7f5fc76215f92738fd
AntonioCruz <antoniocruzglez24@gmail.com>
2024-04-27 00:32:32 -0400

    Pequeños avances con las estadísticas

--- App/static/js/chart_utils.js
+++ App/static/js/chart_utils.js
@@ -16,6 +16,8 @@ const Meses_Label = [
 const Meses_django = ['ene', 'feb', 'mar', 'abr', 'may', 'jun',
     'jul', 'ago', 'sep', 'oct', 'nov', 'dic'];
 
+let indiceMes = 11;
+
 
 function inicializar_meses(){
     const diccionario = {};
@@ -29,18 +31,42 @@ function months_label(mes_actual) {
     return Meses_Label.slice(0, mes_actual)
 }
 
-function addData(chart, label, newData) {
-    chart.data.labels.push(label);
-    chart.data.datasets[0].data[4] = 1
-    chart.data.datasets[1].data[4] = 2
+function mesAnteriorValor(year, mes) {
+    const meses = ["ene", "feb", "mar", "abr", "may", "jun", "jul", "ago", "sep", "oct", "nov", "dic"];
+    const indiceMes = meses.indexOf(mes);
+
+    // Si el mes es enero, el año anterior
+    if (indiceMes === 0) {
+        const YearBefore = (parseInt(year) - 1).toString();
+        const lastMonthYearBefore = datos[YearBefore]["dic"]; // Diciembre del año anterior
+        return { "dic": lastMonthYearBefore };
+    }
+
+    // Si el mes no es enero, simplemente se devuelve el mes anterior
+    const mesAnterior = meses[indiceMes - 1];
+    return { [mesAnterior]: datos[year][mesAnterior] };
+}
+
+
+function addData(chart, newData) {
+    if (chart.data.labels.length < 14){
+        console.log(chart.data.labels[0])
+        chart.data.labels.unshift(Meses_Label[indiceMes]);
+        chart.data.datasets[0].data.unshift(newData[0]);
+        chart.data.datasets[1].data.unshift(newData[1]);
+        indiceMes = (indiceMes -1 + Meses_Label.length) % Meses_Label.length;
+    }
     chart.update();
 }
 
 function removeData(chart) {
-    chart.data.labels.pop();
-    chart.data.datasets.forEach((dataset) => {
-        dataset.data.pop();
-    });
+    if (chart.data.labels.length > 4){
+        chart.data.labels.shift();
+        chart.data.datasets.forEach((dataset) => {
+            dataset.data.shift();
+        });
+        indiceMes++;
+    }
     chart.update();
 }
 
