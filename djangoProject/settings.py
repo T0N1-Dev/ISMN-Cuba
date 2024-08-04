2e73ae27fd63401908305d36b8f2b5fd7f5706ec
AntonioCruz <antoniocruzglez24@gmail.com>
2024-08-03 20:11:34 -0400

    Haciendo deploy 6

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-aweeop%d2l09z%hzb!xmt-&p7fxb9x(+ddtv@u=07y^*5z9@5_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
 
